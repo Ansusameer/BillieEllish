2f0fc6af81b11998393dae9059013bca62f48c90
Rambo999op <111131261+Rambo999op@users.noreply.github.com>
2023-08-06 18:15:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1649,7 +1649,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "shortlink_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("🤵ᴄᴏɴᴛᴀᴄᴛ", url="telegram.me/J_shree_ram")
+                    InlineKeyboardButton("🤵ᴄᴏɴᴛᴀᴄᴛ", url="telegram.me/Rexisop99")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
