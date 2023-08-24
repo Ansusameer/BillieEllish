2625feaf261c8592b3c72b54d3b724ebfe040cb4
DEZIINER <127255670+DEZIINER@users.noreply.github.com>
2023-08-24 17:31:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -7,6 +7,7 @@ import random
 import pytz
 from datetime import datetime, timedelta, date, time
 lock = asyncio.Lock()
+import InputMediaSticker
 
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
