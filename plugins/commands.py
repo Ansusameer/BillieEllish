b4643285904617e5a1c18824f0cfbaeb9e14328a
DEZIINER <127255670+DEZIINER@users.noreply.github.com>
2023-08-24 18:01:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,6 +54,9 @@ async def start(client, message):
                     InlineKeyboardButton('💰 Pʀᴇᴍɪᴜᴍ Pʟᴀɴ 💸', callback_data="shortlink_info")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+        m=await message.reply_sticker("CAACAgQAAxkBAAEKG-Nk50URpkgyBkQ2iBpUVnnZqC7GpwACbg8AAuHqsVDaMQeY6CcRojAE") 
+        await asyncio.sleep(1)
+        await m.delete()        
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
