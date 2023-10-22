8e6ce7199b425f49db97b578d59767b3a9be9e39
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-10-22 19:13:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -182,10 +182,10 @@ async def next_page(bot, query):
     #     InlineKeyboardButton("Hᴏᴡ ᴛᴏ Dᴏᴡɴʟᴏᴀᴅ⚡", url=await get_tutorial(query.message.chat.id))
     # ])
     if settings["button"]:
-        cap = f"<b>🔆 sᴇᴀʀᴄʜ Rᴇꜱᴜʟᴛꜱ Fᴏʀ '{search}'</b>"
+        cap = f"<b>🔆 sᴇᴀʀᴄʜ Rᴇꜱᴜʟᴛꜱ Fᴏʀ '{search}'\n\n</b>"
     else:
         # cap = f"<b>Hᴇʏ {query.from_user.mention}, Hᴇʀᴇ ɪs ᴛʜᴇ ʀᴇsᴜʟᴛ ғᴏʀ ʏᴏᴜʀ ᴏ̨ᴜᴇʀʏ {search} \n\n</b>"
-        cap = f"<b>🔆 sᴇᴀʀᴄʜ Rᴇꜱᴜʟᴛꜱ Fᴏʀ '{search}'</b>"
+        cap = f"<b>🔆 sᴇᴀʀᴄʜ Rᴇꜱᴜʟᴛꜱ Fᴏʀ '{search}'\n\n</b>"
         for file in files:
             cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
 
