8edfe71f0cbe0c93e90e54ef32f32924d6d754a3
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-11-18 15:41:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1579,6 +1579,7 @@ async def auto_filter(client, msg, spoll=False):
             search = re.sub(r"\s+", " ", search).strip()
             search = search.replace("-", " ")
             search = search.replace(":", "")
+            search = search.replace("–", "")
             files, offset, total_results = await get_search_results(message.chat.id ,search, offset=0, filter=True)
             settings = await get_settings(message.chat.id)
             if not files:
