f03a2dec24f8400bdd464df0d6671e15e3a27fb5
Tushar Padir <86295965+PrinceStarLord@users.noreply.github.com>
2024-03-26 19:28:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -36,9 +36,9 @@ SPELL_CHECK = {}
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
-    k = await manual_filters(client, message)
+    # k = await manual_filters(client, message)
     
-    if k is None or k == False:
+    # if k is None or k == False:
         await auto_filter(client, message)
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
