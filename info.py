47eadc8fbe640e04b0bbe0e7573b8a1f76fdafc4
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2024-04-07 19:07:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ DWLD = environ.get("DWLD", 'HeroFlixx/8')
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
-PICS = (environ.get('PICS', 'https://te.legra.ph/file/e709716b0b99ed944a533.jpg https://te.legra.ph/file/e709716b0b99ed944a533.jpg')).split()
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/18cc29953a2084022cea4.jpg https://te.legra.ph/file/18cc29953a2084022cea4.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/e709716b0b99ed944a533.jpg")
 MELCOW_PIC = environ.get("MELCOW_PIC", "https://graph.org/file/75d4fcc66598ea1ef4941.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/2d68d2c0fbf9385d48d70.jpg")
