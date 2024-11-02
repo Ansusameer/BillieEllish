79d49a5912f6dc2b3b2ed1a97ddb1b175f92a1be
Ansusameer <ansusameer@outlook.com>
2024-11-02 21:35:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,10 +47,10 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'vipurl.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'fb07ceec3212e2492512819094d140c247ebb095')
-SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'vipurl.in')
-SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', 'fb07ceec3212e2492512819094d140c247ebb095')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'publicearn.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '9679239790c4e66cc16d5d87e8e847dc59eed4a7')
+SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'publicearn.com')
+SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '9679239790c4e66cc16d5d87e8e847dc59eed4a7')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
